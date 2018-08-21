102ff09036864b477925b6ab4bfc1498a5b2eb59
LesAndreaCarretero <andrea.carretero2097@gmail.com>
2018-08-21 16:55:28 -0500

    Al fin se visualiza mi data -toda info de restaurantes

--- main.js
+++ main.js
@@ -1,44 +1,40 @@
-const viewFood= document.getElementById("allFood");
+const viewFood = document.getElementById("allFood");
 const searchFood = document.getElementById("searchName");
 
 // Utilizaré Xhr 
-const getDataFood = (string, url, callback) => { 
-  const foodXhr = new XMLHttpRequest();
-  foodXhr.open('GET', url, true);
-  foodXhr.addEventListener('load', event => {
-    if (event.target.readyState === 4 && event.target.status === 200) {
-      const response = (JSON.parse(event.target.responseText));
-      callback(string, response);
-    }
-  });
-  foodXhr.send();
-}
-//Aquí se llamará a todos los datos de restaurantes 
-const food = (infoOfFood) => {
-  getDataFood(infoOfFood,`../dataFood/${infoOfFood}/foodDelicious.json`);
-}
-//Variable para pintar en pantalla
-viewFood.addEventListener('onload',(event) => {
-  viewFood.innerHTML= '';
-    Object.keys(allInfoOfood).forEach((id) => {
-      const info = allInfoOfood[id];
-      viewFood.innerHTM += `
-      <div id= ${id}>
-      <h4> ${food.ocassionToEat} </h4>
-      <p > ${food.name} </p>
-      <img src="${food.image}" alt="images1"
-      </div>
-      <p> Distrito: ${food.district} </p>
-      <p> Dirección: ${food.address} </p>
-      <p> Tipo de comida: ${food.typeFood} </p>
-      <p> Teléfonos: ${food.phone}</p>
-      <p> Horarios: ${food.hours}</p>
-      </div>
-      `
-    })
-})
+// const getDataFood = (string, url, callback) => {
+const foodXhr = new XMLHttpRequest();
+foodXhr.open('GET', '../dataFood/foodDelicious.json', true);
+foodXhr.addEventListener('load', event => {
+  if (event.target.readyState === 4 && event.target.status === 200) {
+    const response = (JSON.parse(event.target.responseText));
+    response.map(food => {
+      showAllPlaces(food.name, food.image, food.district, food.address, food.ocassionToEat, food.typeFood, food.phone, food.hours, food.overagePlate);
+    });
+  }
+});
+foodXhr.send();
+// };
 
-    
+//Pintar en pantalla data
+const showAllPlaces = (name, image, district, address, ocassionToEat, typeFood,phone, hours,  overagePlate) => {
+  let template = '';
 
+  template += `
+  <div>
+  <h4> ${ocassionToEat} </h4>
+  <h5 > ${name} </h5>
+  <img src="${image}" alt="images1"
+  </div>
+  <p> Distrito: ${district} </p>
+  <p> Dirección: ${address} </p>
+  <p> Tipo de comida: ${typeFood} </p>
+  <p> Teléfonos: ${phone}</p>
+  <p> Horarios: ${hours}</p>
+  <p> Plato promedio: ${overagePlate}</p>
+  </div>
+  `;
 
+  viewFood.innerHTML += template;
+};
 
